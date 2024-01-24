1195c1d5d6bef35b5af000a20f6df1de4a7665c1
maxwellgithinji <maxwellgithinji@gmail.com>
2024-01-24 16:38:04 -0500

    feat: adds ast for return statemets
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -9,37 +9,44 @@ package ast
 
 import "github.com/maxwellgithinji/jaba/pkg/token"
 
-// Node represents a node in the AST
+// Node represents a single branch in the abstract syntax tree
+// It ensures every method that implements it also returns a token literal
 type Node interface {
 	// TokenLiteral returns the actual value of the token
 	TokenLiteral() string
 }
 
-// Statement represents a structure that does not produce values
+// Statement is structure that abstracts a list of tokens that resemble a single statement
+// Every method that implements Statement should return a token literal (from Node interface) and construct a
+// statement node. Statement does not produce a value. e.g. return 5, return foo
 type Statement interface {
-	// Node ensures each statement implements TokenLiteral
+	// Node ensures each statement returns a token literal
 	Node
 
-	// statementNode constructs a statement node
+	// statementNode constructs a statement node from a combination of tokens that represent a statement
 	statementNode()
 }
 
-// Expression represents a structure that produces values
+// Expression is a structure that abstracts a list of tokens that represent an expression
+// Every method that implements Expression should return a token literal (from Node interface) and construct an
+// expression node. Expression produces a value e.g. return add(1, 2), return 1 + 4
 type Expression interface {
-	// Node ensures each expression implements TokenLiteral
+	// Node ensures each expression returns a token literal
 	Node
 
-	// expressionNode constructs ans expression node
+	// expressionNode constructs an expression node from a combination of tokens that represent an expression
 	expressionNode()
 }
 
-// Program represent the root node of every AST the parser will return
+// Program represents entry point where the root of the AST is initialized and other child nodes are built into the AST
 type Program struct {
-	// Statements contains a list of statements which are building blocks of a program
+	// Statements contains a list of nodes (branches) which are building blocks the AST
 	Statements []Statement
 }
 
-// TokenLiter returns the root node of the AST
+// TokenLiteral method returns the token literal of the first statement in the program.
+// (Root node of the AST)
+// This method is used by the parser to determine the first token to be executed when a program is run.
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -48,19 +55,20 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-// LetStatement should contain the LET token, an identifier and for simplification, an expression.
+// LetStatement defines the  3 parts of a let let statement: "let", "identifier", "expression"
 type LetStatement struct {
-	//  Token is token.LET
+	//  Token is token.LET i.e. {Type: LET, literal: "let"}
 	Token token.Token
 
-	// The Name is the identifier for binding the expression/statement
+	// The Name is the identifier for binding the expression/statement e.g. {token: IDENTIFIER, value: "foo"}
 	Name *Identifier
 
 	// Value represent both the expression and a statement. e.g. "let x = 5" (statement) and "let y = add(2,2)" (expression) can use the expression as the value
 	Value Expression
 }
 
-// statementNode represents a node that does not produce values
+// statementNode method is method that is used to indicate that the LetStatement struct
+// can be used to construct a statement in the Abstract Syntax Tree (AST).
 func (l *LetStatement) statementNode() {}
 
 // TokenLiteral returns the a "let" expression in the AST
@@ -68,19 +76,38 @@ func (l *LetStatement) TokenLiteral() string {
 	return l.Token.Literal
 }
 
-// Identifier contains the structure of an identifier
+// Identifier represents the 2 parts of an identifier, IDENTIFIER and Value e.g. IDENTIFIER("foo")
 type Identifier struct {
 	// Token is the token.IDENTIFIER
 	Token token.Token
 
-	// Value is the actual value the identifier represents
+	// Value is the actual value the identifier represents e.g. "foo"
 	Value string
 }
 
-// expressionNode returns a node that produces a value
+// expressionNode method is method that is used to indicate that the Identifier struct
+// can be used to construct an expression in the Abstract Syntax Tree (AST).
 func (i *Identifier) expressionNode() {}
 
-// TokenLiteral returns the actual identifier value
+// TokenLiteral returns the actual value of the identifier e.g. returns "foo" in IDENTIFIER("foo")
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+// ReturnStatement contains the 2 parts of the return statement, RETURN(expression) e.g. "return add(5,5)"
+type ReturnStatement struct {
+	// Token is the token.RETURN
+	Token token.Token
+
+	// Value is the actual expression being returned e.g. add(5,5), 5, foo, nil. note, we can return both statements and expressions
+	Value Expression
+}
+
+// statementNode method is method that is used to indicate that the ReturnStatement struct
+// can be used to construct a statement in the Abstract Syntax Tree (AST).
+func (r *ReturnStatement) statementNode() {}
+
+// TokenLiteral returns an actual value in a return statement e.g. add(5,5), 5, foo, nil
+func (r *ReturnStatement) TokenLiteral() string {
+	return r.Token.Literal
 }

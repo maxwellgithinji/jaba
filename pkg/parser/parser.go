e8bed32a97bdd0e2fcc94c32b20074165945bd22
maxwellgithinji <maxwellgithinji@gmail.com>
2024-01-27 15:51:50 -0500

    fead: adds boolean expression support
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -46,6 +46,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.INTEGER, p.parseIntegerLiteral)
 	p.registerPrefix(token.NOPE, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.EQ, p.parseInfixExpression)
@@ -377,4 +379,9 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	expression.Right = p.parseExpression(precedences)
 
 	return expression
+}
+
+// Parse boolean uses go boolean syntax to parse the value of the expression
+func (p *Parser) parseBoolean() ast.Expression {
+	return &ast.Boolean{Token: p.currentToken, Value: p.currentTokenIS(token.TRUE)}
 }

ee26b5f629371af78d2a97e2f66fa2411429489c
maxwellgithinji <maxwellgithinji@gmail.com>
2024-01-29 17:48:03 -0500

    feat: upgrades read eval print lexer to read eval print parser
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -11,11 +11,22 @@ import (
 	"io"
 
 	"github.com/maxwellgithinji/jaba/pkg/lexer"
-	"github.com/maxwellgithinji/jaba/pkg/token"
+	"github.com/maxwellgithinji/jaba/pkg/parser"
 )
 
 // Prompt indicates the user start typing jaba code.
 const Prompt = ">>"
+
+// PRETTY_JABA a pretty printer that prints jaba logo
+const PRETTY_JABA = `
+____    
+/oo  \   
+|   __/    
+/    _ |    
+|     \ \    
+\___  \ \__ 
+|     \___\
+`
 
 // Run is a Read Eval Print Loop function that runs the jaba program.
 // it helps the user code the jaba program on the command line
@@ -31,8 +42,25 @@ func Run(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 		l := lexer.New(line)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, PRETTY_JABA)
+	io.WriteString(out, "Woops! We ran into some jaba stories here!\n")
+	io.WriteString(out, "parser errors: \n")
+	for _, message := range errors {
+		io.WriteString(out, "\t"+message+"\n")
 	}
 }

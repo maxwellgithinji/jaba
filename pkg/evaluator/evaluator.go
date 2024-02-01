89b9e99149281f2a64a21bf82d84dbc257d8ef8a
maxwellgithinji <maxwellgithinji@gmail.com>
2024-02-01 17:28:35 -0500

    feat: adds evaluation of a function node
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- pkg/evaluator/evaluator.go
+++ pkg/evaluator/evaluator.go
@@ -71,6 +71,11 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.IfExpression:
 		return evalIfExpression(node, env)
 
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+
 	// Identifier
 	case *ast.Identifier:
 		return evalIdentifier(node, env)

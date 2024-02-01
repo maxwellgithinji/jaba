6e4c44f25532f857621d9ed8870c6ec6fb16f7ee
maxwellgithinji <maxwellgithinji@gmail.com>
2024-02-01 17:26:56 -0500

    feat: adds object representation of a function
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- pkg/object/object.go
+++ pkg/object/object.go
@@ -5,7 +5,13 @@
  */
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/maxwellgithinji/jaba/pkg/ast"
+)
 
 // ObjectType represents the category of the object
 type ObjectType string
@@ -16,6 +22,7 @@ const (
 	NULL_OBJECT         = "NULL"
 	RETURN_VALUE_OBJECT = "RETURN_VALUE"
 	ERROR_OBJECT        = "ERROR"
+	FUNCTION_OBJECT     = "FUNCTION_OBJECT"
 )
 
 // Object is an interface that helps represent the values encountered when evaluating the jaba program
@@ -86,7 +93,7 @@ func (r *ReturnValue) Type() ObjectType {
 	return RETURN_VALUE_OBJECT
 }
 
-// Inspect returns the representation of the object value, return value
+// Inspect returns the string representation of the object value, return value
 func (r *ReturnValue) Inspect() string {
 	return r.Value.Inspect()
 }
@@ -105,4 +112,42 @@ func (e *Error) Type() ObjectType {
 // Inspect returns the string representation of the object value, error
 func (e *Error) Inspect() string {
 	return "ERROR: " + e.Message
+}
+
+// Function represents a jaba function and may include parameters and some statements to be executed
+// it fulfills the Object interface by implementing the Type() and Inspect() methods
+type Function struct {
+	// Parameters is a list of identifiers that should be passed to the function call
+	Parameters []*ast.Identifier
+
+	// Body contains a list of function statements to be evaluated
+	Body *ast.BlockStatement
+
+	// Env keeps track of variables during interpreter execution
+	Env *Environment
+}
+
+// Type returns the type of the object, function
+func (f *Function) Type() ObjectType {
+	return FUNCTION_OBJECT
+}
+
+// Inspect returns the string representation of the function
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+
+	for _, param := range f.Parameters {
+		params = append(params, param.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
 }

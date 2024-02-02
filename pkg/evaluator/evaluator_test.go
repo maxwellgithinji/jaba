d108ead88d875379f96f006ee03c6a9290d6beab
maxwellgithinji <maxwellgithinji@gmail.com>
2024-02-01 19:27:53 -0500

    test: adds support for function evaluation
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- pkg/evaluator/evaluator_test.go
+++ pkg/evaluator/evaluator_test.go
@@ -283,3 +283,34 @@ func TestFunctionObject(t *testing.T) {
 		t.Fatalf("function.Body is not %q, got: %q", expectedBody, function.Body.String())
 	}
 }
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x) {x}; identity(5);", 5},
+		{"let identity = fn(x) {return x}; identity(5);", 5},
+		{"let double = fn(x) {return x * 2}; double(5);", 10},
+		{"let add = fn(x, y) {return x + y}; add(5, 5);", 10},
+		{"let add = fn(x, y) {return x + y}; add(5 + 5, add(5, 5));", 20},
+		{"fn(x) { x; }(5)", 5},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
+func TestClosures(t *testing.T) {
+	input := `
+	let newAdder = fn(x) {
+		fn(y) {x + y };
+	};
+
+	let addTwo = newAdder(2);
+	addTwo(2);
+	`
+
+	testIntegerObject(t, testEval(input), 4)
+}

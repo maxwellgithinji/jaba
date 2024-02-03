37206bc736a1bd20c34659f7fea6c97fd0a99855
maxwellgithinji <maxwellgithinji@gmail.com>
2024-02-02 20:30:53 -0500

    feat: adds len builtin function evaluation
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- pkg/evaluator/evaluator_test.go
+++ pkg/evaluator/evaluator_test.go
@@ -350,3 +350,38 @@ func TestStringConcatenation(t *testing.T) {
 	}
 
 }
+
+func TestBuiltinFunctions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`len("");`, 0},
+		{`len("four");`, 4},
+		{`len("hello world")`, 11},
+		{`len(1);`, "argument to len not supported, got: INTEGER"},
+		{`len("one", "two")`, "wrong number of arguments. got: 2 want: 1"},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t, evaluated, int64(expected))
+
+		case string:
+			errorObject, ok := evaluated.(*object.Error)
+			if !ok {
+				t.Fatalf("evaluated is not *object.Error, got: %T(%+v)", evaluated, evaluated)
+				continue
+			}
+			if errorObject.Message != expected {
+				t.Errorf("errorObject.Message is not %s, got %s", expected, errorObject.Message)
+			}
+
+		default:
+
+		}
+	}
+}

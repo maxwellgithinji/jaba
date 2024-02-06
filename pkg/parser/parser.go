c8e1120b773d7b3a45cc46d938d6749243a4518e
maxwellgithinji <maxwellgithinji@gmail.com>
2024-02-06 17:45:58 -0500

    feat: adds parsing of hash literals
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -53,6 +53,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 	p.registerPrefix(token.STRING, p.parseStringLiteral)
 	p.registerPrefix(token.LBRACKET, p.parseArrayLiteral)
+	p.registerPrefix(token.LBRACE, p.parseHashLiteral)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.EQ, p.parseInfixExpression)
@@ -582,4 +583,37 @@ func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
 	}
 
 	return expression
+}
+
+// parseHashLiteral returns a representation of a hash literal value
+func (p *Parser) parseHashLiteral() ast.Expression {
+	hashLiteral := &ast.HashLiteral{Token: p.currentToken}
+
+	hashLiteral.Pairs = make(map[ast.Expression]ast.Expression)
+
+	for !p.peekTokenIs(token.RBRACE) {
+		p.nextToken()
+
+		key := p.parseExpression(LOWEST)
+
+		if !p.expectPeek(token.COLON) {
+			return nil
+		}
+
+		p.nextToken()
+
+		value := p.parseExpression(LOWEST)
+
+		hashLiteral.Pairs[key] = value
+
+		if !p.peekTokenIs(token.RBRACE) && !p.expectPeek(token.COMMA) {
+			return nil
+		}
+	}
+
+	if !p.expectPeek(token.RBRACE) {
+		return nil
+	}
+
+	return hashLiteral
 }

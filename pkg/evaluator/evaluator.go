f18cb5c984d6b5edea5c981f402f3455f434d15e
maxwellgithinji <maxwellgithinji@gmail.com>
2024-02-06 19:30:50 -0500

    feat: evaluates jaba hashes
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- pkg/evaluator/evaluator.go
+++ pkg/evaluator/evaluator.go
@@ -111,6 +111,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 		return evalIndexExpression(left, index)
 
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
+
 	// Identifier
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
@@ -419,4 +422,33 @@ func evalArrayIndexExpression(array, index object.Object) object.Object {
 	}
 
 	return arrayObject.Elements[indexValue]
+}
+
+// evalHashLiteral evaluates jaba hash literals
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+
+	for keyNode, valueNode := range node.Pairs {
+		key := Eval(keyNode, env)
+		if isError(key) {
+			return key
+		}
+
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return newError("unable to hash key:  %s", key.Type())
+		}
+
+		value := Eval(valueNode, env)
+		if isError(value) {
+			return value
+		}
+
+		hashed := hashKey.HashKey()
+
+		pairs[hashed] = object.HashPair{Key: key, Value: value}
+
+	}
+
+	return &object.Hash{Pairs: pairs}
 }
